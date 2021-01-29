7d9621524e8a086a77ddbb339ce1a86048d0eeb5
MrCoderboy <57526988+MrCoderboy@users.noreply.github.com>
2021-01-29 16:25:00 +0400

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {RouterModule, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'india-auzzio-general-site';
+export class AppComponent implements OnInit {
+  title = 'auzzio-password-reset';
+  constructor(private router: Router) {
+  }
+  ngOnInit(){
+    this.router.navigateByUrl('/password')
+  }
+
 }
