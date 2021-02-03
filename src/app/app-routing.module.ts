72284d6ef772d57cb68c787b10335bf73d5b82a9
sujithk04 <51526468+sujithk04@users.noreply.github.com>
2021-02-03 13:48:29 +0400

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ResetpasswordpublicComponent} from './resetpasswordpublic/resetpasswordpublic.component';
-import {AppComponent} from './app.component';
-import {EmailverifylinkComponent} from './emailverifylink/emailverifylink.component';
+import { ResetpasswordpublicComponent } from './resetpasswordpublic/resetpasswordpublic.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [ {
+const routes: Routes = [{
   path: 'reset/password/:resetkey',
   component: ResetpasswordpublicComponent,
 },
-  { path: '', redirectTo: '/password/:resetkey', pathMatch: 'full' },
-  { path: '**', component: AppComponent }
+{ path: '', redirectTo: '/password/:resetkey', pathMatch: 'full' },
+{ path: '**', component: AppComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResetpasswordpublicComponent } from './resetpasswordpublic/resetpasswordpublic.component';
-import {HelperServicesService} from './services/helper-services.service';
-import {DeviceDetectorService} from 'ngx-device-detector';
-import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import { HelperServicesService } from './services/helper-services.service';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { EmailverifylinkComponent } from './emailverifylink/emailverifylink.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +22,7 @@ import { EmailverifylinkComponent } from './emailverifylink/emailverifylink.comp
   imports: [
     BrowserModule,
     AppRoutingModule, MatToolbarModule,
-    BrowserAnimationsModule , MatButtonModule,
+    BrowserAnimationsModule, MatButtonModule,
     MatFormFieldModule, MatInputModule, MatCardModule,
     ReactiveFormsModule
   ],
